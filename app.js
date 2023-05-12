7fbeba5607d07d4af9d9fe3d76e41e2fb44e9758
cyrine25 <amricyrine@gmail.com>
2023-05-12 18:03:12 +0200

    work in progress

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express')
+var cors = require('cors')
 var app = express()
-
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 app.get('/questions', (req, res) => {
   res.sendFile(__dirname + '/data/questions.json')
 })
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
-})
+app.listen(5000, err =>
+  err ? console.error(err) : console.log(`server is running on port 5000`)
+)
